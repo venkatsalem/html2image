20bfacb4604e1d0ddc102bd788b073e6edb14d20
Venkat Sadasivam <venkatsalem@gmail.com>
2023-11-10 09:28:42 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sharp = require('sharp'); // Import the sharp library
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.text({type: 'text/html'}));
 
 const maxRequestsBeforeNewBrowser = 1000;
 let browserInstance = null;
@@ -51,6 +51,38 @@ app.post('/html-to-image', async (req, res) => {
         console.error('Error:', error);
         res.status(500).send('An error occurred while capturing and trimming the image.');
     }
+});
+
+
+app.post('/html-to-pdf', async (req, res) => {
+    const htmlContent = req.body;
+
+    if (!htmlContent) {
+        return res.status(400).send('Please provide HTML content in the request body.');
+    }
+
+    try {
+        const browser = await getBrowserInstance();
+        const page = await browser.newPage();
+
+        // Set the HTML content from the request
+        await page.setContent(htmlContent);
+
+        // Generate a PDF from the HTML content
+        const pdfBuffer = await page.pdf();
+
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename=generated.pdf');
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred while generating the PDF.');
+    }
+});
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).send('Server is healthy');
 });
 
 app.listen(port, () => {
